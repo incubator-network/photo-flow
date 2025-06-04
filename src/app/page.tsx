aecbb6cea656883ae7959b027187cef46504d530
Valery Bruner <vbruner11@mail.ru>
2025-06-04 18:54:34 +0700

    feat: add Typography component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import { Typography } from '@/components/typography/Typography'
+
 export default function Home() {
   return (
-    <div className={'bg-success-900 font-sans text-h1 text-center mt-20'}>
-      Hello this a test string
+    <div>
+      <div className={'bg-danger-700 font-sans text-h1 text-center mt-20'}>
+        Hello this a test string
+      </div>
+      <Typography variant={'h1'} className={'text-center mt-5 bg-danger-700'}>
+        Typography example
+      </Typography>
     </div>
   )
 }
