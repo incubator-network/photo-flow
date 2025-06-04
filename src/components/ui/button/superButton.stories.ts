a10da1354c645bcc4e57686ab30312b2d3f62d33
Moi-glaza-vezde <moi.glaza.vezde@gmail.com>
2025-06-04 11:24:03 +0500

    added to storybook Secondary Outline Text, changed colors in button

--- src/components/ui/button/superButton.stories.ts
+++ src/components/ui/button/superButton.stories.ts
@@ -1,17 +1,34 @@
 import { Meta } from '@storybook/nextjs-vite'
-import {SuperButton} from '@/components/ui/button/superButton'
+import { SuperButton } from '@/components/ui/button/superButton'
 
 const meta = {
   component: SuperButton,
 } satisfies Meta<typeof SuperButton>
 export default meta
 
-export const Button = {
-
+export const Primary = {
   args: {
     variant: 'primary',
     children: 'Button',
-
-  }
-
+  },
 }
+export const Secondary = {
+  args: {
+    variant: 'secondary',
+    children: 'Button',
+  },
+}
+export const Outline = {
+  args: {
+    variant: 'outline',
+    children: 'Button',
+  },
+}
+export const Text = {
+  args: {
+    variant: 'text',
+    children: 'Button',
+  },
+}
+
+// variant: 'primary' | 'secondary' | 'outline' | 'text',
--- src/components/ui/button/superButton.tsx
+++ src/components/ui/button/superButton.tsx
@@ -1,75 +1,81 @@
 // 'use client'
 
-import React, {ReactNode} from 'react';
+import React, { ReactNode } from 'react'
 
 type PropsType = {
-  variant: 'primary' | 'secondary' | 'outline' | 'text',
-  children?: ReactNode,
-  onClick?: () => void,
-  disabled?: boolean,
-  type?: 'button' | 'submit' | 'reset',
+  variant: 'primary' | 'secondary' | 'outline' | 'text'
+  children?: ReactNode
+  onClick?: () => void
+  disabled?: boolean
+  type?: 'button' | 'submit' | 'reset'
 }
 const variantStyles = {
   primary: `
                flex items-center justify-center 
-               text-base text-[#FFFFFF]  font-[600]
+               text-base 
+               text-[var(--color-light-100)]  
+               font-[600]
                w-[182px] h-[36px] 
-               bg-[#397DF6]               
-               hover:bg-[#73A5FF] 
-               focus:bg-[#2F68CC] 
+               bg-[var(--color-accent-500)]               
+               hover:bg-[var(--color-accent-100)] 
+               focus:bg-[var(--color-accent-700)] 
                focus-visible:outline-1
-               focus-visible:outline-[#2F68CC]
-               active:bg-[#2F68CC] 
-               disabled:bg-[#234E99]
-               disabled:text-[#8D9094] 
+               focus-visible:outline-[var(--color-accent-700)]
+               active:bg-[var(--color-accent-700)] 
+               disabled:bg-[var(--color-accent-900)]
+               disabled:text-[var(--color-light-900)] 
                rounded-[2px]`,
   secondary: `
               flex items-center justify-center 
-              text-[#FFFFFF]
+              text-[var(--color-light-100)]  
               font-[600]
-              text-base
+              text-base 
               w-[182px] h-[36px]  
-              bg-[#333333] 
-               rounded-[2px]
-              hover:bg-[#4C4C4C]
-              focus-visible:bg-[##333333]
+              bg-[var(--color-dark-300)] 
+              rounded-[2px]
+              hover:bg-[var(--color-dark-100)]
+              focus-visible:bg-[var(--color-dark-300)]
               focus-visible:outline-1
-              focus-visible:outline-[#4C8DFF]
+              focus-visible:outline-[var(--color-accent-300)]
               active:bg-[#212121] 
-              disabled:bg-[#171717] disabled:text-[#8D9094]`,
+              disabled:bg-[var(--color-dark-500)] 
+              disabled:text-[var(--color-light-900)]`,
   outline: `
              flex items-center justify-center
-             text-base w-[182px] h-[36px]
-             text-[#397DF6]
+             text-base 
+             w-[182px] h-[36px]
+             text-[var(--color-accent-500)]
              font-[600]
-             border border-[#397DF6] rounded-[2px]
-             hover:border-[#73A5FF]
-             hover:text-[#73A5FF]
-             focus:outline-[#2F68CC] focus:text-[#2F68CC]
-             active:border-[#2F68CC] active:text-[#2F68CC]
-             disabled:border-[#234E99] disabled:text-[#234E99]`,
+             border border-[var(--color-accent-500)] 
+             rounded-[2px]
+             hover:border-[var(--color-accent-100)]
+             hover:text-[var(--color-accent-100)]
+             focus:outline-[var(--color-accent-700)] focus:text-[var(--color-accent-700)]
+             active:border-[var(--color-accent-700)] active:text-[var(--color-accent-700)]
+             disabled:border-[var(--color-accent-900)] 
+             disabled:text-[var(--color-accent-900)]`,
   text: `
              flex items-center justify-center
              text-base w-[100px] h-[36px]
-             text-[#397DF6]
+             text-[var(--color-accent-500)]
              font-[600]
-             border border-transparent rounded-[2px]
-             hover:text-[#73A5FF]
-             focus:outline-[#2F68CC] focus:text-[#2F68CC]
-             active:text-[#2F68CC]
-             disabled:text-[#234E99]`,
-};
-
+             rounded-[2px]
+             hover:text-[var(--color-accent-100)]
+             focus:outline-[var(--color-accent-700)] focus:text-[var(--color-accent-700)]
+             focus-visible:outline-1
+             active:text-[var(--color-accent-700)]
+             disabled:text-[var(--color-accent-900)]`,
+}
 
 export const SuperButton = (props: PropsType) => {
-  const {variant,children, onClick} = props;
+  const { variant, children, onClick, disabled } = props
   return (
     <button
       onClick={onClick}
       className={variantStyles[variant]}
-      disabled={false}>
+      disabled={disabled}
+    >
       {children}
     </button>
-  );
-};
-
+  )
+}
