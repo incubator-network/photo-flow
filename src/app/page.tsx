0b6cd5f0350a1895354dd1607ef1223964771dc5
Valery Bruner <vbruner11@mail.ru>
2025-06-05 14:38:05 +0700

    refactor: add twMerge + story for LinkAsButton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { Typography } from '@/components/typography/Typography'
+import { Button } from '@/components/ui/button/Button'
 
 export default function Home() {
   return (
@@ -9,6 +10,18 @@ export default function Home() {
       <Typography variant={'h1'} className={'text-center mt-5 bg-danger-700'}>
         Typography example
       </Typography>
+      <Button>
+        <Typography variant={'h1'}>Button example</Typography>
+      </Button>
+      <Button variant={'secondary'}>
+        <Typography variant={'h1'}>Button example</Typography>
+      </Button>
+      <Button variant={'outline'}>
+        <Typography variant={'h1'}>Button example</Typography>
+      </Button>
+      <Button variant={'text'}>
+        <Typography variant={'h1'}>Button example</Typography>
+      </Button>
     </div>
   )
 }
