d7a86f4ca9160d995cb49c65c275227c06e7aadc
Moi-glaza-vezde <moi.glaza.vezde@gmail.com>
2025-06-06 15:00:13 +0500

    style(scrollbar): added custom border to scrollbar thumb using native CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Card } from '@/components/ui/superCard/Card'
 
 export default function Home() {
   return (
-    <div>
+    <div style={{ height: '2000px', width: '120%' }}>
       <div className={'bg-danger-700 font-sans text-h1 text-center mt-20'}>
         Hello this a test string
       </div>
