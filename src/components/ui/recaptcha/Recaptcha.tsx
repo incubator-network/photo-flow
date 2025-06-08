999c45ef6965df4da620e573818b40e8ec1c3718
semaxio <kutchms@gmail.com>
2025-06-08 13:31:51 +0300

    PF-14 feat: add Recaptcha

---
+++ src/components/ui/recaptcha/Recaptcha.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import ReCAPTCHA from 'react-google-recaptcha'
+import { twMerge } from 'tailwind-merge'
+import { RefObject } from 'react'
+import { Typography } from '@/components/ui/typography/Typography'
+
+type RecaptchaProps = {
+  publicKey: string
+  theme?: 'dark' | 'light'
+  size?: 'normal' | 'compact' | 'invisible'
+  lang?: 'en' | 'ru'
+  error: boolean
+  ref: RefObject<ReCAPTCHA | null>
+  handleCaptchaAction: (token: string | null) => void
+}
+
+export const Recaptcha = ({
+  publicKey,
+  lang = 'en',
+  size = 'normal',
+  theme = 'dark',
+  error,
+  ref,
+  handleCaptchaAction,
+}: RecaptchaProps) => {
+  return (
+    <div
+      className={twMerge(
+        'w-fit flex-col px-[7px] py-[8px]',
+        error ? 'border border-danger-500' : ''
+      )}
+    >
+      <ReCAPTCHA
+        ref={ref}
+        sitekey={publicKey}
+        theme={theme}
+        size={size}
+        hl={lang}
+        onChange={handleCaptchaAction}
+      />
+      {error && (
+        <Typography variant={'regular_text_14'} className={'text-danger-500'}>
+          Please verify that you are not a robot
+        </Typography>
+      )}
+    </div>
+  )
+}
