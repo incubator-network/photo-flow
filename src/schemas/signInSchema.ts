cec6f0a7bdfc27bb79df814452031e5b2fe18903
Dzmitry Yarasheuski <materik08082020@gmail.com>
2025-06-12 20:46:00 +0300

    feat(auth): add zod validation schema to sign-in form

---
+++ src/schemas/signInSchema.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod'
+const specialChars = `!\"#$%&'()*+,-./:;<=>?@[\\]^_\`{|}~`
+
+export const signInSchema = z.object({
+  email: z
+    .string()
+    .min(1, 'Email is required')
+    .email('The email must match the format example@example.com'),
+  password: z
+    .string()
+    .min(6, 'The password must be at least 6 characters long')
+    .max(20, 'The password must not exceed 20 characters.')
+    .regex(
+      /^(?=.*[A-Za-z])(?=.*\d).+$/,
+      `Password must contain at least one special character: ${specialChars}`
+    ),
+})
+
+export type TSignInSchema = z.infer<typeof signInSchema>
