fa30e7852c1f2d1f1c89626d1857f32fbf23be87
“den987655” <den987655@gmail.com>
2025-06-14 12:12:07 +0300

    feat: add logout

--- src/app/logout/page.tsx
+++ src/app/logout/page.tsx
@@ -1,10 +1,30 @@
-import { ModalLogout } from '@/components/ui/ModalLogout/ModalLogout'
+'use client'
+import { useLogoutMutation } from '@/lib/api/authApi'
+import { Button } from '@/components/ui/button/Button'
 
 export default function Logout() {
+  const [logout] = useLogoutMutation()
+
+  const logoutHandler = async () => {
+    await logout()
+      .then(res => {
+        console.log(res)
+        if (res.data === '204') console.log(res, 'Logout successful')
+      })
+      .catch(error => {
+        console.log(error.data.messages)
+      })
+  }
   return (
-    <>
+    <div>
       <p>header</p>
-      <ModalLogout />
-    </>
+      <Button
+        variant='text'
+        className='text-light-100 font-semibold'
+        onClick={logoutHandler}
+      >
+        Logout
+      </Button>
+    </div>
   )
 }
