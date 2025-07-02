2f8fd0992d68169920e7a9557ca4c2f60c9a9dd8
Dzmitry Yarasheuski <materik08082020@gmail.com>
2025-07-03 01:54:14 +0300

    fix: update the description to the current one

--- src/lib/feature/posts/ui/post/PostModal.tsx
+++ src/lib/feature/posts/ui/post/PostModal.tsx
@@ -4,6 +4,7 @@ import { ModalWindow } from '@/components/ui/modalWindow/ModalWindow'
 import {
   Comment,
   getPostInformation,
+  getPostResponse,
   UpdatePostMutation,
 } from '@/lib/feature/posts/api/postsApi.types'
 import { useRouter } from 'next/navigation'
@@ -19,35 +20,30 @@ import { selectIsAuth } from '@/lib/appSlice'
 import { useEffect, useState } from 'react'
 import { Textarea } from '@/components/ui/textarea/Textarea'
 import { Button } from '@/components/ui/button/Button'
-import { useGetPostByIdQuery, useUpdatePostMutation } from '../../api/postsApi'
+import { useUpdatePostMutation } from '../../api/postsApi'
 
 type PropsType = {
-  postId: number
+  post: getPostResponse
   comments: getPostInformation<Comment[]>
 }
 
-export default function PostModal({ postId, comments }: PropsType) {
-  // export default function PostModal({ post, comments }: PropsType) {
+export default function PostModal({ post, comments }: PropsType) {
   const [isEditMode, setIsEditMode] = useState<boolean>(false)
-  const { data: postData, refetch } = useGetPostByIdQuery(postId)
 
-  console.log(postData)
   const [textValue, setTextValue] = useState<string>('')
 
   useEffect(() => {
-    if (postData?.description) {
-      setTextValue(postData.description)
+    if (post?.description) {
+      setTextValue(post.description)
     }
-  }, [postData])
+  }, [post])
   const router = useRouter()
 
   const isAuth = useAppSelector(selectIsAuth)
   const [updatePost] = useUpdatePostMutation() as UpdatePostMutation
-  if (!postData) return null // потом поменять на лоадер. Защита от postData = undefined
 
   const onCloseHandler = () => {
     if (!isEditMode) {
-      console.log('zalupa')
       return router.back()
     }
 
@@ -58,17 +54,17 @@ export default function PostModal({ postId, comments }: PropsType) {
 
   return (
     <ModalWindow open onClose={onCloseHandler} className={'flex h-[565px] w-[972px]'}>
-      <Slider images={postData.images} />
+      <Slider images={post.images} />
       <div className={'relative'}>
         <header className={'flex items-center gap-3 px-6 py-3'}>
           <Image
             width={36}
             height={36}
-            src={postData.avatarOwner}
+            src={post.avatarOwner}
             className={'rounded-full'}
             alt={'photo of creator'}
           />
-          <Typography variant={'h3'}>{postData.userName}</Typography>
+          <Typography variant={'h3'}>{post.userName}</Typography>
           {isAuth && !isEditMode && (
             <Dots
               className={'fill-accent-500 absolute right-6 h-6 w-6 cursor-pointer'}
@@ -83,16 +79,15 @@ export default function PostModal({ postId, comments }: PropsType) {
             // нужен ли перенос слов????
             className={`border-dark-100 mb-3 ${isAuth ? 'max-h-[336px]' : 'max-h-[420px]'} w-[480px] overflow-y-auto border-t pt-5 pr-4 pl-6`}
           >
-            {postData.description && <PostDescription post={postData} />}
+            {post.description && <PostDescription post={{ ...post, description: textValue }} />}
             {comments &&
-              comments.items.map(c => <PostComment key={c.id} postId={postId} comment={c} />)}
+              comments.items.map(c => <PostComment key={c.id} postId={post.id} comment={c} />)}
           </section>
         ) : (
           <form
             onSubmit={async e => {
               e.preventDefault()
-              await updatePost({ description: textValue, postId })
-              await refetch()
+              await updatePost({ description: textValue, postId: post.id })
               setIsEditMode(false)
             }}
             className='flex h-full w-full flex-col justify-between'
@@ -110,7 +105,7 @@ export default function PostModal({ postId, comments }: PropsType) {
           </form>
         )}
 
-        {!isEditMode && <PostFooter post={postData} />}
+        {!isEditMode && <PostFooter post={post} />}
       </div>
     </ModalWindow>
   )
