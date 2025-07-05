b46521e213f44f8f990d0a8dcd1eee266e61e073
Daniil <daniil.shishaev@gmail.com>
2025-07-05 15:25:56 +0300

    feat:Expandable text component created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,18 @@ import PublicPosts from '@/lib/feature/publicPage/ui/components/PublicPosts/Publ
 import { UsersCounter } from '@/lib/feature/publicPage/ui'
 
 export default async function HomePage() {
+  const POSTS_ON_PAGE = 4
   const postsRaw = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/public-posts/all?pageSize=4`,
-    {
-      next: { revalidate: 10 },
-    }
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/public-posts/all?pageSize=${POSTS_ON_PAGE}`
   )
-  const countRaw = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/public-user`, {
-    next: { revalidate: 10 },
-  })
+  const countRaw = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/public-user`)
   const publicPosts = await postsRaw.json()
   const usersCount = await countRaw.json()
 
   return (
     <div className={'flex flex-col items-center'}>
       <UsersCounter usersCount={usersCount.totalCount} />
-      <PublicPosts initialPosts={publicPosts} />
+      <PublicPosts initialPosts={publicPosts} POSTS_ON_PAGE={POSTS_ON_PAGE} />
     </div>
   )
 }
